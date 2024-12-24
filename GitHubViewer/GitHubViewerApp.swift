294ce41a55b802bacfb37bb1f685deb90516d0c5
Adrian Truszczyński <adrian.truszczynski@gmail.com>
2024-12-24 07:36:28 +0100

    Initial commit.

---
+++ GitHubViewer/GitHubViewerApp.swift
@@ -0,0 +1,15 @@
+// --------------------------------------------------
+// GitHubViewerApp.swift
+// Copyright © 2024 Adrian Truszczyński.
+// All Rights Reserved.
+// --------------------------------------------------
+
+
+import SwiftUI
+
+@main
+struct GitHubViewerApp: App {
+    var body: some Scene {
+        WindowGroup {}
+    }
+}
